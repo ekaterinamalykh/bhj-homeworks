7a8cae621ddebfcb44fbe2af4d9a051dcd98d02c
Ekaterina Malykh <minikat21@gmail.com>
2020-08-12 21:17:43 +0300

    second commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,46 +1,25 @@
 const deadMoles = document.getElementById("dead");
 const lostMoles = document.getElementById("lost");
-const firstHole = document.getElementById("hole1");
-const classHoles = firstHole.className;
-firstHole.onclick = function () {
-  if (firstHole.className.includes("hole_has-mole")) {
-    deadMoles.textContent++;
-  } else {
-    lostMoles.textContent++;
-  }
-  if (deadMoles.textContent == 10) {
-    alert("Победа!");
-    deadMoles.textContent = 0;
-  }
-  if (lostMoles.textContent == 5) {
-    alert("Поражение!");
-    lostMoles.textContent = 0;
-  }
-};
+const holes = document.getElementsByClassName("hole");
 
-
-// let timer = setInterval(firstHole.onclick);
-// if (deadMoles.textContent == 10 || lostMoles.textContent == 5) {
-//   clearInterval(timer);
-// }
-
-// const deadMoles = document.getElementById("dead");
-// const lostMoles = document.getElementById("lost");
-// const hole = document.getElementById(`hole${index}`);
-// hole.onclick = function () {
-//   if (hole.className.includes("hole_has-mole")) {
-//     deadMoles.textContent++;
-//   } else {
-//     lostMoles.textContent++;
-//   }
-//   if (deadMoles.textContent == 10) {
-//     alert("Победа!");
-//     deadMoles.textContent = 0;
-//   }
-//   if (lostMoles.textContent == 5) {
-//     alert("Поражение!");
-//     lostMoles.textContent = 0;
-//   }
-// };
+for (let i = 0; i < holes.length; i++) {
+  holes[i].onclick = function () {
+    if (holes[i].className.includes("hole_has-mole")) {
+      deadMoles.textContent++;
+    } else {
+      lostMoles.textContent++;
+    }
+    if (deadMoles.textContent == 10) {
+      alert("Победа!");
+      deadMoles.textContent = 0;
+      lostMoles.textContent = 0;
+    }
+    if (lostMoles.textContent == 5) {
+      alert("Поражение!");
+      lostMoles.textContent = 0;
+      deadMoles.textContent = 0;
+    }
+  };
+}
 
 
