afab0a1a0e67aedddfe78a3adee80eb1dca873de
Ekaterina Malykh <minikat21@gmail.com>
2020-08-12 20:02:48 +0300

    second commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,11 @@
 const competitionTimer = function () {
   const seconds = document.getElementById("timer");
   seconds.textContent -= 1;
+
+  if (seconds.textContent == 0) {
+    alert("Вы победили в конкурсе!");
+    clearInterval(competitionTimer);
+  }
 };
 setInterval(competitionTimer, 1000);
-setTimeout(() => {clearInterval(competitionTimer); alert("Вы победили в конкурсе!");}, 60000);
+
