b47fb9c5b2f5bb892142ec371650b707b6885130
Ekaterina Malykh <minikat21@gmail.com>
2020-09-26 19:40:22 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,17 @@
 const tooltips = document.getElementsByClassName("has-tooltip");
+const tooltip = document.getElementsByClassName("tooltip");
 
-function showTip() {
-  event.preventDefault();
-  this.insertAdjacentHTML("afterEnd", "<div class='tooltip'></div>");
-  this.nextElementSibling.classList.add('tooltip_active');
-  this.nextElementSibling.textContent = this.title;
-  let coords = this.getBoundingClientRect();
-  this.nextElementSibling.style.left = coords.left + "px";
+for (let i = 0; i < tooltips.length; i++) {
+  tooltips[i].onclick = function() {
+    event.preventDefault();
+    if (tooltip.length == 0) {
+      this.insertAdjacentHTML("afterEnd", "<div class='tooltip'></div>");
+      this.nextElementSibling.textContent = this.title;
+      this.nextElementSibling.classList.add('tooltip_active');
+      let coords = this.getBoundingClientRect();
+      this.nextElementSibling.style.left = coords.left + "px";
+    } else {
+        tooltip[0].remove();
+    }
+  }
 }
-
-for (let tooltip of tooltips) {
-  tooltip.addEventListener("click", showTip); 
-}
-
