32e9f9478580712b03ec599d32d4834789e715b7
Ekaterina Malykh <minikat21@gmail.com>
2020-10-10 15:37:24 +0300

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,26 +3,23 @@ const tasksList = document.getElementById("tasks__list");
 const button = document.getElementById("tasks__add");
 const tasks = document.getElementById("tasks");
 
-function addTask() {
+function addTask(event) {
+  event.preventDefault();
   if (taskPlace.value.length > 0) {
-    event.preventDefault();
     tasks.insertAdjacentHTML('beforeEnd', '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>');
-    let text = document.getElementsByClassName("task__title")[0];
-    text.append(taskPlace.value);
+    tasks.lastElementChild.firstElementChild.innerText = taskPlace.value;
     taskPlace.value = "";
     let deleteButtons = document.getElementsByClassName("task__remove");
     for (let deleteButton of deleteButtons) {
-        deleteButton.onclick = () => deleteButton.parentElement.remove();
+      deleteButton.onclick = () => deleteButton.parentElement.remove();
     }
   }
 }
+
 button.addEventListener('click', addTask);
 
 taskPlace.addEventListener("keyup", function(e) {
-  if (e.keyCode === 13 && taskPlace.value.length > 0) {
-    event.preventDefault();
-    tasks.insertAdjacentHTML('beforeEnd', '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>');
-
+  if (e.keyCode === 13) {
+    addTask;
   }
 })
-
