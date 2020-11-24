7086b0e40eaf82a8dc3fea9b4b5f69b8044d18dc
Ekaterina Malykh <minikat21@gmail.com>
2020-11-24 21:56:35 +0300

    third commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -5,21 +5,14 @@ const tasks = document.getElementById("tasks");
 
 function addTask(event) {
   event.preventDefault();
+  taskPlace.value = taskPlace.value.trim();
   if (taskPlace.value.length > 0) {
     tasks.insertAdjacentHTML('beforeEnd', '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>');
     tasks.lastElementChild.firstElementChild.innerText = taskPlace.value;
     taskPlace.value = "";
-    let deleteButtons = document.getElementsByClassName("task__remove");
-    for (let deleteButton of deleteButtons) {
-      deleteButton.onclick = () => deleteButton.parentElement.remove();
-    }
+    let deleteButton = tasks.lastElementChild.getElementsByClassName("task__remove")[0];
+    deleteButton.onclick = () => deleteButton.parentElement.remove();
   }
 }
 
 button.addEventListener('click', addTask);
-
-taskPlace.addEventListener("keyup", function(e) {
-  if (e.keyCode === 13) {
-    addTask;
-  }
-})
